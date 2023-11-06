c783fdb9c315de8a59b1e8b7cd9d1708a8a69c1e
Anna Tyshchenko <annati4nko@gmail.com>
2023-11-06 09:31:29 +0200

    second commit

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const server = http.createServer((req, res) => {
     const xmlData = fs.readFileSync('data.xml');
     const parser = new XMLParser();
     const jsonData = parser.parse(xmlData);
-    const currencies = jsonData.exchange.currency;
+
+    let currencies = jsonData.exchange.currency;
+    if (!Array.isArray(currencies)) {
+        currencies = [currencies];
+    }
+
     const newXMLData = {
         data: {
             exchange: currencies.map((currency) => ({
-                date: currency.exchangedate,
-                rate: currency.rate,
+                currency: {
+                    date: currency.exchangedate,
+                    rate: currency.rate,
+                },
             })),
         },
     };
 
-    const builder = new XMLBuilder({});
+    const builder = new XMLBuilder({}); 
     const xmlres = builder.build(newXMLData);
 
     res.setHeader('Content-Type', 'text/xml');
@@ -25,5 +32,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-    console.log(`Сервер:http://localhost:8000`);
+    console.log(`Сервер: http://localhost:8000`);
 });
